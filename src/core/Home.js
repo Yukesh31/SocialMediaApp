1c8832832e3d5dca6424380f4749996fb5f16c02
Yukesh kumaran <86478248+Yukesh31@users.noreply.github.com>
2022-01-09 23:15:37 +0530

    Add files via upload

---
+++ src/core/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Posts from '../post/Posts';
+
+const Home = () => (
+    <>
+        <div className="container">
+            <Posts />
+        </div>
+        <footer className="page-footer font-small" style={{ background: "#3E4551" }}>
+            <div className="container">
+                <p className="text-center" style={{ color: "#fff", fontSize: "large", margin: "0", padding: "20px" }}>
+                    <i>Made with <i className="fas fa-heart" style={{ color: "red", fontSize: "24px" }}></i> by
+                        Yukesh </i>
+                </p>
+            </div>
+        </footer>
+    </>
+);
+
+export default Home;
